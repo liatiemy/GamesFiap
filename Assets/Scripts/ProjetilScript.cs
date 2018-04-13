5411ab3976a09560504b8f4103ecc375f34263f6
Marilia Tiemy <liazinha.tiemy@gmail.com>
2018-04-12 22:21:34 -0300

    Criacao do projetil, arma e player prefabs

---
+++ Assets/Scripts/ProjetilScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilScript : MonoBehaviour {
+
+	public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		//Destroi caso o objeto nao acerte nada
+		Destroy (gameObject, 3.0f);
+		
+	}
+
+	private void OnCollisionEnter2d(Collision2D c){
+		//Destroi o objeto por colisão
+		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		//como o peixinho eh trigger nao precisa colocar c.gameobject.tag
+		if (c.tag == "Inimigo") {
+			Destroy(c.gameObject);
+		}
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		//Movendo o tiro
+		transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/armaScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class armaScript : MonoBehaviour {
+	public GameObject projetilPrefab;
+	public GameObject sensorRotacao;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Instantiate (projetilPrefab, transform.position, transform.rotation);
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") > 0.0f) {
+			sensorRotacao.transform.eulerAngles = new  Vector3(0.0f,0.0f,0.0f);
+		} else if (Input.GetAxisRaw ("Horizontal") < 0.0f) {
+			sensorRotacao.transform.eulerAngles = new  Vector3(0.0f,180.0f,0.0f);
+		}
+	}
+}
