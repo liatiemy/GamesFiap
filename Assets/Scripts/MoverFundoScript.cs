263bfdae76c5a39cf39eec23488bc7fa88912a96
meiregoncalves <30304639+meiregoncalves@users.noreply.github.com>
2018-04-22 16:17:44 -0300

    mover fundo

---
+++ Assets/Scripts/MoverFundoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverFundoScript : MonoBehaviour {
+
+	public float velocidade;
+	public float posicaoInicial, posicaoFinal;
+
+	// Update is called once per frame
+	void Update () {
+
+		//verifica se atingiu o limite da camera e reposiciona para um novo ciclo
+		if (transform.position.x <= posicaoFinal) {
+			transform.position = new Vector2 (posicaoInicial, transform.position.y);
+		}
+
+		//move o fundo para a esquerda
+		transform.Translate (Vector2.left * velocidade * Time.deltaTime);
+	}
+}
